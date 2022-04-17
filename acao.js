885635f90916c1129b7c469365e2f73376953a54
Larissa Paixao <laryssapaixao9@gmail.com>
2022-04-16 22:22:00 -0300

    melhora no layout e correção no recebimento de mensagem de saida

--- acao.js
+++ acao.js
@@ -7,22 +7,28 @@ const url3="https://mock-api.driven.com.br/api/v6/uol/status"
 let pegarUsuario;
 let sendServidor;
 let ggServidor;
+let rUser;
+let User;
+
 
 
 function entrar_sala(){
     User={name:prompt("Digite seu nome de usuario")}
 
+  
     pegarUsuario=axios.post(url2,User);
 
     pegarUsuario.catch(entrar_sala);
     pegarUsuario.then((resultado)=>{
         verificar()})
     }
-entrar_sala();
+//entrar_sala();
 
 function continuar_verficando(){
     rUser=axios.post(url3,User);
-   
+    rUser.then((response)=>{
+        
+    })
     rUser.catch((error)=>{
         window.location.reload();
     })
@@ -48,26 +54,36 @@ function renderizarChat(response){
         
         containerMensagem.innerHTML+=`<div class="mensagem cinza">${response.data[i].time} <b>${response.data[i].from}</b> ${response.data[i].text}</div>`
     }
+    if(response.data[i].text=="sai da sala..." && response.data[i].type=="status"){
+        
+        containerMensagem.innerHTML+=`<div class="mensagem cinza">${response.data[i].time} <b>${response.data[i].from}</b> ${response.data[i].text}</div>`
+    }
     if(response.data[i].type=="message" && response.data[i].to=="Todos"){
-        containerMensagem.innerHTML+=`<div class="mensagem white">${response.data[i].time}<b>${response.data[i].from} </b><span> para <b>todos</b>:</span> ${response.data[i].text}</div>` 
+        //containerMensagem.innerHTML+=`<div class="mensagem white">${response.data[i].time}<b>${response.data[i].from} </b><span> para <b>todos</b>:</span> ${response.data[i].text}</div>` 
+        containerMensagem.innerHTML+=`<div class="mensagem white">${response.data[i].time}  <b>${response.data[i].from} </b><span> para <b>todos</b>:</span> ${response.data[i].text}</div> `
     }
     if(response.data[i].type=="message" && response.data[i].to!="Todos"){
         containerMensagem.innerHTML+=`<div class="mensagem white">${response.data[i].time}<b>${response.data[i].from} </b><span> para <b>${response.data[i].to}</b>:</span> ${response.data[i].text}</div>` 
     }
-    if(response.data[i].type=="private_message"){
+    if(response.data[i].type=="private_message" && response.data[i].to==User.name){
         containerMensagem.innerHTML+=`<div class="mensagem reservada">${response.data[i].time}<b>${response.data[i].from} </b><span> reservadamente para: <b>${response.data[i].to}</b>:</span> ${response.data[i].text}</div>` 
     }
+    if(response.data[i].type=="private_message" && response.data[i].to!=User.name){
+        containerMensagem.innerHTML+="";
     }
-
+    }
+    let mensagemFinal=document.querySelector(".container")
+    mensagemFinal[mensagemFinal.length-1].scrollIntoView()
+    
 }
 setInterval(()=>{
     const atualizarMensagens=axios("https://mock-api.driven.com.br/api/v6/uol/messages");
     atualizarMensagens.then(renderizarChat);
 
+
 },3000)
 
-    const mensagemFinal=document.querySelector(".mensagem");
-    mensagemFinal.scrollIntoView(false);
+
 
 function enviar_mensagem(){
 
@@ -90,6 +106,10 @@ function enviar_mensagem(){
     
 }
 
+
+const mensagemFinal=document.querySelector(".mensagem");
+mensagemFinal.scrollIntoView(false);
+//termina aqui!
 
 
 
